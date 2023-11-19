1c7953002654236b93a5fc7d168cb734d639bce9
Ivana Jankovic <ivanaj0610@gmail.com>
2023-11-19 15:17:40 +0100

    Fix dependencies

--- bank-acquirer/src/main/java/com/project/bankacquirer/config/Config.java
+++ bank-acquirer/src/main/java/com/project/bankacquirer/config/Config.java
@@ -1,8 +1,6 @@
 package com.project.bankacquirer.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
