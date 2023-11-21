d6b2486a95698f2b7953985fd139292a90ef7df6
Ivana Jankovic <ivanaj0610@gmail.com>
2023-11-21 11:45:08 +0100

    Configure issuer bank data

---
+++ bank-issuer/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO accounts (number, balance)
+VALUES ('12345678', '10000');
+
+INSERT INTO merchants (id, username, password, merchant_id, merchant_password, account)
+VALUES (nextval('user_seq'), 'prodavac2', 'pass', 'nekiId2', 'pass', 1);
+
+
+INSERT INTO accounts (number, balance)
+VALUES ('87654321', '10000');
+
+INSERT INTO clients (id, username, password, account)
+VALUES (nextval('user_seq'), 'klijent', 'pass', 2);
+
+INSERT INTO credit_cards (pan, security_code, cardholder_name, expiration_month, expiration_year, account)
+VALUES ('2222333344445555', '111', 'Ivana Jankovic', '07', '27', 2);
+
